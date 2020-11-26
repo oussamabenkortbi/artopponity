72c7a2f1e2e751ea594bf64c2806e713de227b4f
Oussama Benkortbi <amassou@amassou.local>
2020-11-26 01:16:21 +0100

    deploy

---
+++ client/build/precache-manifest.e28dd50f1c876b2b4ee44053418123e4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe867949c4b15631fb041aa7dbbae63f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc618459712aed35afb8",
+    "url": "/static/css/2.dee4ed97.chunk.css"
+  },
+  {
+    "revision": "346a5bb8c52467ded7fa",
+    "url": "/static/css/main.233f5fdf.chunk.css"
+  },
+  {
+    "revision": "dc618459712aed35afb8",
+    "url": "/static/js/2.9b9e76f8.chunk.js"
+  },
+  {
+    "revision": "1a3cc71403f332d098a2b9cdd7c4c36d",
+    "url": "/static/js/2.9b9e76f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "346a5bb8c52467ded7fa",
+    "url": "/static/js/main.734a5344.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "47d62805a1deced93d235caa4658ccb4",
+    "url": "/static/media/logo.47d62805.svg"
+  }
+]);
